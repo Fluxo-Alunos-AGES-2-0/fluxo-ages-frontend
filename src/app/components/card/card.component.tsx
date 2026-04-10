ed6882ee1bbdf6778acf821743a629421aad52c5
lpmilitao <luizamilitaop@gmail.com>
2026-04-10 20:51:34 -0300

    adicionando mudanças para ficar sem o header

--- src/app/components/card/card.component.tsx
+++ src/app/components/card/card.component.tsx
@@ -1,11 +1,11 @@
-import React, { ImgHTMLAttributes, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import styles from './card.module.css';
 import pencil from '../../../assets/edit-pencil.svg'
 import clockIcon from "../../../assets/clock.svg"
 import arrowIcon from "../../../assets/rising-arrow.svg"
 
 interface CardProps {
-  title: string;
+  title?: string;
   icon?: string;
   headerAction?: ReactNode;
   children: ReactNode;
@@ -13,28 +13,32 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ title, icon, headerAction, children, className }) => {
-  const hasHeader = title || headerAction;
+  const hasHeader = !!(title || headerAction); // Força um booleano
 
   return (
-    <div className={`${styles.cardContainer} ${className || ''}`}>
-        <div className={styles.blueTopBorder} />
+    <div className={`
+      ${styles.cardContainer} 
+      ${!hasHeader ? styles.noHeader : ''} 
+      ${className || ''}
+    `}>
+      <div className={styles.blueTopBorder} />
 
       {hasHeader && (
         <div className={styles.cardHeader}>
           <div className={styles.titleGroup}>
-            {icon == "clock" ? 
-              <img src={clockIcon} alt="Clock icon in the color blue"/>
-            : icon == "arrow" ? 
-              <img src={arrowIcon} alt="Rising arrow icon in the collor blue"/>
-            : null}
+            {icon === "clock" && <img src={clockIcon} alt="Clock icon"/>}
+            {icon === "arrow" && <img src={arrowIcon} alt="Rising arrow icon"/>}
             {title && <h2 className={styles.title}>{title}</h2>}
           </div>
-          {headerAction && <div className={styles.action}>
-            <img src={pencil} alt="Pencil icon in the collor grey" />
-            {headerAction}
-            </div>}
+          {headerAction && (
+            <div className={styles.action}>
+              <img src={pencil} alt="Edit icon" />
+              {headerAction}
+            </div>
+          )}
         </div>
       )}
+
       <div className={styles.cardContent}>
         {children}
       </div>
