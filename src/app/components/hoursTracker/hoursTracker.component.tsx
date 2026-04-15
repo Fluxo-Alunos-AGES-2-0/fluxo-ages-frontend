2a16a57a55fb4c0cad642811049331314d76b488
Theo Miguel <theogmiguel@gmail.com>
2026-04-15 19:46:28 -0300

    fix: consolidate loader CSS, fix invalid card CSS, rename setter convention

--- src/app/components/hoursTracker/hoursTracker.component.tsx
+++ src/app/components/hoursTracker/hoursTracker.component.tsx
@@ -1,62 +1,68 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import style from './hoursTracker.module.css'
+import style from "./hoursTracker.module.css";
 
 import Card from "../card/card.component";
-import CircularProgress from '../circularProgress/circularProgress.component';
-import Loader from '../loader/loader.component';
+import CircularProgress from "../circularProgress/circularProgress.component";
+import Loader from "../loader/loader.component";
 
 const HoursTracker = () => {
   const [hours, setHours] = useState({
     total: "",
     done: "",
-    todo: ""
+    todo: "",
   });
-  const [loading, isLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   const fetchHoursData = () => {
     setTimeout(() => {
-      console.log("Fingindo que estamos esperando dados do backend");
       setHours({
         total: "60:00:00",
         done: "42:00:00",
-        todo: "18:00:00"
+        todo: "18:00:00",
       });
 
-      isLoading(false);
+      setLoading(false);
     }, 3000);
-  }
+  };
 
   useEffect(() => {
     fetchHoursData();
   }, []);
 
-  return(
-    <Card title="Controle de Horas" icon="arrow" className="fullWidth" classContent={style.hoursTrackerContent}>
-    {
-      loading ? (
+  return (
+    <Card
+      title="Controle de Horas"
+      icon="arrow"
+      className="fullWidth"
+      classContent={style.hoursTrackerContent}
+    >
+      {loading ? (
         <Loader />
       ) : (
         <>
-        <div className={style.hourSection}>
-          <span className={`${style.hourCount} ${style.blue}`}>{hours.done}</span>
-          <p className={style.hourCountTitle}>Concluídas</p>
+          <div className={style.hourSection}>
+            <span className={`${style.hourCount} ${style.blue}`}>
+              {hours.done}
+            </span>
+            <p className={style.hourCountTitle}>Concluídas</p>
           </div>
-        <div className={style.hourSection}>
-          <span className={`${style.hourCount} ${style.orange}`}>{hours.todo}</span>
-          <p className={style.hourCountTitle}>A cumprir</p>
-        </div>
-        <div className={style.hourSection}>
-          <span className={style.hourCount}>{hours.total}</span>
-          <p className={style.hourCountTitle}>Total</p>
-        </div>
-        <div className={style.hourSection}>
-          <CircularProgress percentage={65}/>
-        </div>
+          <div className={style.hourSection}>
+            <span className={`${style.hourCount} ${style.orange}`}>
+              {hours.todo}
+            </span>
+            <p className={style.hourCountTitle}>A cumprir</p>
+          </div>
+          <div className={style.hourSection}>
+            <span className={style.hourCount}>{hours.total}</span>
+            <p className={style.hourCountTitle}>Total</p>
+          </div>
+          <div className={style.hourSection}>
+            <CircularProgress percentage={65} />
+          </div>
         </>
-        )
-      }
+      )}
     </Card>
-  )
-}
+  );
+};
 export default HoursTracker;
