b799d15808aa23cfe758a747a29171e48650c79d
Theo Miguel <theogmiguel@gmail.com>
2026-04-16 18:58:08 -0300

    fix: remove duplicate TimerProvider from App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import { TimerProvider } from "./context/TimerContext";
 
 export default function App() {
-  return (
-    <TimerProvider>
-      <RouterProvider router={router} />
-    </TimerProvider>
-  );
+  return <RouterProvider router={router} />;
 }
