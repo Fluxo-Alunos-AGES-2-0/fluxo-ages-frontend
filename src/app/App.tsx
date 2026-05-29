d7660524337a4c38f19269e8ae91bbfacc259a94
Ana Lech <analech@dohms.org.br>
2026-05-29 15:17:11 -0300

    correão no App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,10 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from "react-hot-toast";
-import { TeacherFeedbackModal } from "./components/reports/TeacherFeedbackModal"; // 1. Adicione o import
 
 export default function App() {
   return (
     <>
-    <TeacherFeedbackModal 
-        isOpen={true} // Força ele a ficar aberto na tela sem precisar logar
-        onClose={() => console.log("Fechando...")} 
-        reportData={{
-           }}
-      />
       <Toaster position="top-right" containerStyle={{ zIndex: 99999 }} />
       <RouterProvider router={router} />
     </>
